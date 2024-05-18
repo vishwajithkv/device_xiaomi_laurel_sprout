a7551fd2b248e19cc5c848e280bb95d29a183ac1
Arian <arian.kulmer@web.de>
2024-05-18 15:52:45 +0200

    laurel_sprout: Provide a static lib for power mode extension
    Change-Id: I3e2cf17bf1e733838e9e18afdf3f888e814593dc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,7 +119,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := trinket
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
