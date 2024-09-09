b81579f78f49fef10af5f44646ffee03328dd7ff
Vishwajith K V <86662074+vishwajithkv@users.noreply.github.com>
2024-09-09 05:48:20 +0000

    laurel_sprout: RisingOS bringup

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -4,16 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
 
+# RisingOS
+RISING_MAINTAINER := leoxvis(vishwajithkv)
+RISING_CHIPSET := Snapdragon665
+PRODUCT_NO_CAMERA := false
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_MAINTAINER="leoxvis(vishwajithkv)"
+
+# Device Identifier
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_NAME := lineage_laurel_sprout
 PRODUCT_BRAND := Xiaomi
