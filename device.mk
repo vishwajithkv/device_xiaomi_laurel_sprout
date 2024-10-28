4b340ab3c0c859658a6c90c9eb58723d0387eea8
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-28 22:07:01 +0100

    laurel_sprout: Support UDFPS on AoD
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -264,6 +264,13 @@ PRODUCT_PACKAGES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.udfps
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
