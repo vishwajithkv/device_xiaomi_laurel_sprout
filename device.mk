82e31213e4d1430476462fe1c24abec0c4febe11
basamaryan <basam.aryan@gmail.com>
2025-01-09 15:56:49 -0500

    laurel_sprout: media: Import Codec2 configs
    From sunfish TQ3A.230805.001.S1
    
    Change-Id: Ie2ecfa912e0fe0892a2e6b8ddc3a6b6aa3213d40
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -177,6 +177,8 @@ PRODUCT_PACKAGES += \
 
 # Media configs
 PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_performance_c2.xml \
     media_codecs.xml \
     media_codecs_vendor.xml \
     media_codecs_vendor_audio.xml \
