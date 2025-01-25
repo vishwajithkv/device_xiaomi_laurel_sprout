e04b2133f5b6c70c82095bf89cdc789a24da030c
Skyblueborb <tomaszborbely0710@gmail.com>
2025-01-25 12:30:29 +0100

    laurel_sprout: media: Import Codec2 from sweet
    sm6150 and sm6125 share the same encoders/decoders, meaning we can
    migrate to C2 as well.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ TARGET_USES_FOD_ZPOS := true
 # HIDL
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
