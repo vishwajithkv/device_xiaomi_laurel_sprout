f7fddf0d7bd6b12e9a3f5f55c445b4eb301ecbc4
Skyblueborb <tomaszborbely0710@gmail.com>
2025-02-13 17:06:05 +0100

    laurel_sprout: Switch to OSS audio HAL
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
--- extract-files.py
+++ extract-files.py
@@ -57,8 +57,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libhidltransport.so', 'libhidlbase-v32.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
-    'vendor/lib/hw/audio.primary.laurel_sprout.so': blob_fixup()
-        .binary_regex_replace(b"vendor/lib/liba2dpoffload.so", b"liba2dpoffload_laurel_sprout.so")
 }  # fmt: skip
 
 module = ExtractUtilsModule(
